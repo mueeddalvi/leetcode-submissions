3c70530de7b4f521d3dd7bae7e4f17f21caad28a
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2020-09-10 08:47:30 +0000

    [LeetCode Sync] Runtime - 8 ms (7.18%), Memory - 9.9 MB (100.00%)

---
+++ my-folder/0134-gas-station/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size(),tank=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            tank+=gas[i]-cost[i];
+        }
+        
+        if(tank<0)
+            return -1;
+        int i=0,start=0;;
+        tank=0;
+        for(;i<n;i++)
+        {
+            tank+=gas[i]-cost[i];
+            if(tank<0)
+            {
+                start=i+1;
+                tank=0;
+            }
+        }
+        
+        return start;
+        
+    }
+};
