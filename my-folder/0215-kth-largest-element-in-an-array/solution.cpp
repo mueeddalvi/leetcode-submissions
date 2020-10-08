6a19068abc1092c089037ecc7f55bd8fc68507e4
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2020-10-08 07:47:27 +0000

    [LeetCode Sync] Runtime - 28 ms (70.14%), Memory - 10.4 MB (100.00%)

---
+++ my-folder/0215-kth-largest-element-in-an-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<int,vector<int>,greater<int>>h;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            h.push(nums[i]);
+            if(h.size()>k)
+                h.pop();
+        }
+        
+        return h.top();
+            
+        
+    }
+};
