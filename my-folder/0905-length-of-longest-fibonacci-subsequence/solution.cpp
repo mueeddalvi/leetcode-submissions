9e70a5e56b857ea7ac72d9c0cb7410e641b50793
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2020-11-04 06:34:16 +0000

    [LeetCode Sync] Runtime - 456 ms (7.32%), Memory - 8.9 MB (100.00%)

--- my-folder/0905-length-of-longest-fibonacci-subsequence/solution.cpp
+++ my-folder/0905-length-of-longest-fibonacci-subsequence/solution.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& a) {
-        int n=a.size(),i,j,k,index,res=0;
-        if(n<3)
+        
+        int n=a.size(),i,j,k,index,res=0,l=0;
+        if(n<=3)
             return res;
-        unordered_map<int,int>m;
- 
-        int dp[n][n];
-        for(j=0;j<n;j++)
+        unordered_set<int>s(a.begin(),a.end());
+        int x=0,y=0;
+        
+        for(int i=0;i<a.size();i++)
         {
-            m[a[j]]=j;
-            for(i=0;i<j;i++)
+            for(j=i+1;j<a.size();j++)
             {
-                int k=-1;
-                if(m.find(a[j]-a[i])!=m.end())
-                    k=m[a[j]-a[i]];
-                if(a[j]-a[i]<a[i] and k>=0)
+                x=a[i];
+                y=a[j];
+                l=2;
+                while(s.count(x+y))
                 {
-                    dp[i][j]=dp[k][i]+1;
+                    y=x+y;
+                    x=y-x;
+                    l++;
                 }
-                else 
-                    dp[i][j]=2;
-                res=max(res,dp[i][j]);
+                res=max(res,l);
             }
         }
-        if(res>2)
-            return res;
-        else
-            return 0;
+        return res;
+        
+//         unordered_map<int,int>m;
+ 
+//         int dp[n][n];
+//         for(j=0;j<n;j++)
+//         {
+//             m[a[j]]=j;
+//             for(i=0;i<j;i++)
+//             {
+//                 int k=-1;
+//                 if(m.find(a[j]-a[i])!=m.end())
+//                     k=m[a[j]-a[i]];
+//                 if(a[j]-a[i]<a[i] and k>=0)
+//                 {
+//                     dp[i][j]=dp[k][i]+1;
+//                 }
+//                 else 
+//                     dp[i][j]=2;
+//                 res=max(res,dp[i][j]);
+//             }
+//         }
+//         if(res>2)
+//             return res;
+//         else
+//             return 0;
 
     }
 };
