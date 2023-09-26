c1a181c0f079602b1d22a951168f00b8b42930c2
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2023-09-26 13:24:57 +0000

    [LeetCode Sync] Runtime - 5 ms (82.39%), Memory - 55.5 MB (99.92%)

---
+++ my-folder/0011-container-with-most-water/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int res=0,i=0,j=height.length-1;
+
+        while(i<j){
+            res=Math.max((Math.min(height[i],height[j])*(j-i)),res);
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return res;
+    }
+}
