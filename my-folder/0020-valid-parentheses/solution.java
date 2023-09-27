9bada247ff42e18e944d3f72e1ab3b0c139390b2
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2023-09-27 13:28:57 +0000

    [LeetCode Sync] Runtime - 2 ms (98.10%), Memory - 40.4 MB (100.00%)

---
+++ my-folder/0020-valid-parentheses/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        if(s.charAt(0)==')' || s.charAt(0)=='}' || s.charAt(0)==']')
+            return false;
+        Stack<Character> stack=new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+                stack.push(c);
+            else if(!stack.empty()){
+                if(c==')' && stack.pop()!='(')
+                    return false;
+                else if(c=='}' && stack.pop()!='{')
+                    return false;
+                else if(c==']' && stack.pop()!='[')
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        if(!stack.empty())
+            return false;
+
+        return true;
+        
+    }
+}
