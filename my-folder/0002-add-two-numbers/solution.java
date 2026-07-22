f97be983d31c48bfbc4377e4bfa850fe73077b91
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2026-07-22 14:07:15 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 46.5 MB (57.58%)

--- my-folder/0002-add-two-numbers/solution.java
+++ my-folder/0002-add-two-numbers/solution.java
@@ -9,72 +9,42 @@
  * }
  */
 class Solution {
-    List<Integer> res=new ArrayList<>();
-    int carry=0;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        while(l1!=null && l2!=null){
-            int input=l1.val+l2.val+carry;
-            if(input>9){
-                carry=1;
-            }
-            else{
-                carry=0;
-            }
-            res.add(input%10);
-            System.out.print(l1.val+" + "+l2.val+"\n");
-            l1=l1.next;
-            l2=l2.next;
+
+        ListNode curr1 = l1, curr2 = l2, curr3 = null;
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        curr3 = head;
+        while (curr1 != null && curr2 != null) {
+            int sum = curr1.val + curr2.val + carry;
+            carry = sum / 10;
+            curr3.next = new ListNode(sum % 10);
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+            curr3 = curr3.next;
         }
-        System.out.println();
-        while(l1!=null){
-            int input=l1.val;
-            if(carry>0){
-                input+=carry;
-            }
-            if(input>9){
-                carry=1;
-            }
-            else{
-                carry=0;
-            }
-            res.add(input%10);
-            l1=l1.next;
+
+        while (curr1 != null) {
+            int sum = curr1.val + carry;
+            carry = sum / 10;
+            curr3.next = new ListNode(sum % 10);
+            curr1 = curr1.next;
+            curr3 = curr3.next;
+
         }
-        while(l2!=null){
-            int input=l2.val;
-            if(carry>0){
-                input+=carry;
-            }
-            if(input>9){
-                carry=1;
-            }
-            else{
-                carry=0;
-            }
-            l2=l2.next;
-            res.add(input%10);
+
+        while (curr2 != null) {
+            int sum = curr2.val + carry;
+            carry = sum / 10;
+            curr3.next = new ListNode(sum % 10);
+            curr2 = curr2.next;
+            curr3 = curr3.next;
+
         }
-        if(carry==1){
-            res.add(1);
-        }
-        //708 ==> 807
-        ListNode temp=new ListNode();
-        ListNode start=new ListNode();
-        start=temp;
-        for(int i=0;i<res.size();i++){
-            System.out.print(res.get(i)+" ");
-            temp.val=res.get(i);
-            if(i!=res.size()-1){
-                temp.next=new ListNode();
-                temp=temp.next;
-            }
-        }
-        return start;
+
+        if(carry>0)
+            curr3.next=new ListNode(carry);
+        return head.next;
+
     }
 }
-
-/*
-[9,9,9,9,9,9,9]
-      [9,9,9,9]
-           ,9,8  
- */
